37fbd9d5d2f856c385463f84a3ba481e646a377d
yossef ashraf <youssef.ashraf@mitchdesigns.com>
2024-11-11 15:51:52 +0200

    Implement cookie retrieval function and re-enable AJAX call for government selection

--- wp-content/themes/mitchdesigns/backend_functions.js
+++ wp-content/themes/mitchdesigns/backend_functions.js
@@ -2447,33 +2447,46 @@ if(!full_address.trim()){
 console.log("Js Loaded ");
 
 
+function getCookie(name) {
+  let nameEQ = name + "=";
+  let ca = document.cookie.split(';');
+  for (let i = 0; i < ca.length; i++) {
+      let c = ca[i].trim();
+      if (c.indexOf(nameEQ) === 0) {
+          return decodeURIComponent(c.substring(nameEQ.length, c.length));
+      }
+  }
+  return false;
+}
 
 
-// $(document).ready(function() {
-//   console.log("Js Loaded 1");
-//   const $firstRadio = $('.gover-checkbox').first(); 
-//   if ($firstRadio.length) {
-//     $("#branch_submit button").attr("disabled", true);
-//     $('#ajax_loader').show();  
-//     const gover_en = $firstRadio.attr('id')
-//     if (gover_en != null) {
-//       $.ajax({
-//         type: 'POST',
-//         dataType: 'JSON',
-//         url: mitch_ajax_url,
-//         data: {
-//             action: "MD_get_areas_related_gover",
-//             gover_en: gover_en ,
-//             lang : current_lang ,
-//         },
-//         success: function (data) {
+$(document).ready(function() {
+  console.log("Js Loaded 1");
+  const $firstRadio = $('.gover-checkbox').first(); 
+  if ($firstRadio.length) {
+    const gover_en = $firstRadio.attr('id')
+    let branchId = getCookie('branch_id') ?? false ;
+    // alert(branchId);
+    if (gover_en != null && branchId == false) {
+      $("#branch_submit button").attr("disabled", true);
+      $('#ajax_loader').show();  
+      $.ajax({
+        type: 'POST',
+        dataType: 'JSON',
+        url: mitch_ajax_url,
+        data: {
+            action: "MD_get_areas_related_gover",
+            gover_en: gover_en ,
+            lang : current_lang ,
+        },
+        success: function (data) {
     
-//           $('#area').html(data.areas_dropdown);
-//           $('#street').html(data.street);
-//           $('#ajax_loader').hide();  
-//         }
-//       })
-//     }
-// }
+          $('#area').html(data.areas_dropdown);
+          $('#street').html(data.street);
+          $('#ajax_loader').hide();  
+        }
+      })
+    }
+}
 
-// });
+});
